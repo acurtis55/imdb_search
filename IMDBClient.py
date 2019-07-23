545a798fecbfb243c72eae9a8e74a3047c834403
Alexandria Curtis <alexandria@Alexandrias-MacBook-Pro-2.local>
2019-07-23 18:58:31 -0500

    Used IMDBClient to display hardcoded search results

---
+++ IMDBClient.py
@@ -0,0 +1,38 @@
+import urllib3
+from bs4 import BeautifulSoup
+
+
+
+class IMDBClient:
+
+    def __init__(self):
+        self.url = "http://www.imdb.com/search/title?"
+
+
+    def __getMovies__(self, searchUrl):
+
+        dataUrl = urllib3.PoolManager().request('GET', searchUrl).data
+        soup = BeautifulSoup(dataUrl, "html.parser")
+
+        movieList = soup.findAll('div', attrs={'class': 'lister-item mode-advanced'})
+
+        movieTitles = []
+
+        for divItem in movieList:
+            div = divItem.find('div', attrs={'class': 'lister-item-content'})
+
+            header = div.findChildren('h3', attrs={'class':'lister-item-header'})
+            movieTitles.append(str((header[0].findChildren('a'))[0].contents[0].encode('utf-8').decode('ascii', 'ignore')))
+        return movieTitles
+
+    def searchByReleaseYear(self, releaseYear):
+
+        print("Searching by release year: ", releaseYear)
+        searchUrl = self.url + "release_date=" + releaseYear
+        return self.__getMovies__(searchUrl)
+
+    def searchByTitle(self, title):
+
+        print("Searching by title: ", title)
+        searchUrl = self.url + "title=" + title
+        return self.__getMovies__(searchUrl)
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from forms import IMDBSearchForm
+from IMDBClient import IMDBClient
 
 app = Flask(__name__)
+app.secret_key = "my secret key"
+imdb_client = IMDBClient()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    search_form = IMDBSearchForm()
+    search_form = IMDBSearchForm(request.form)
+    if request.method == "POST":
+        print(search_form.data['search'])
+        results = imdb_client.searchByReleaseYear('2012')
+        return render_template("results.html", form=search_form, movies=results)
     return render_template("index.html", form=search_form)
-
 
 if __name__ == "__main__":
     app.run()
